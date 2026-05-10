502294b17b055f110b8a4015a86539eb1845ef6e
prem-web.dev <a70064182@gmail.com>
2026-05-10 23:09:47 +0530

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,10 @@
-import { clsx } from clsx;
-// No clsx in deps, I'll define a simple cn function.
 export function cn(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
 export function formatDate(date) {
-  return new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric' }).format(new Date(date));
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+  }).format(new Date(date));
 }
